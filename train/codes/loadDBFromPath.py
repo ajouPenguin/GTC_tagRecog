ec6b754ff67f990cd96e079af81ec8fa2086925d
hyeon <ch0104@ajou.ac.kr>
2017-11-26 00:40:34 +0900

    add non-filtered code in run.py

--- train/codes/loadDBFromPath.py
+++ train/codes/loadDBFromPath.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy as np
 from regionProposal import extractFeature
 from brightChange import brightChange
 
@@ -16,7 +17,8 @@ def loadDBFromPath(path, classnum, isFiltered=1):
         if isFiltered :
             data['feat'] = extractFeature(img)
         else :
-            data['feat'] = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY),(-1, 1)
+            data['feat'] = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+            data['feat'] = np.resize(data['feat'], -1)
         db.append(data)
         if classnum == 1:
             for i in range(15):
@@ -24,8 +26,8 @@ def loadDBFromPath(path, classnum, isFiltered=1):
                     data['feat'] = extractFeature(brightChange(img, 0))
                 else :
                     tmp = brightChange(resized, 0)
-                    data['feat'] = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY),(-1, 1)
-
+                    data['feat'] = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
+                    data['feat'] = np.resize(data['feat'], -1)
                 db.append(data)
 
     return db
